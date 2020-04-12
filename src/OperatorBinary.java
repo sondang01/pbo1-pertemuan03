347dd72eab02f34b5fc199b1e309c8216abb83da
sondang01 <zamsondang@gmail.com>
2020-04-12 21:43:05 +0800

    Operaor Binary Assignment

---
+++ src/OperatorBinary.java
@@ -0,0 +1,12 @@
+public class OperatorBinary {
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Zam Sondang Fuadzi";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+
+
+}
