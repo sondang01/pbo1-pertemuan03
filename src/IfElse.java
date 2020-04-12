f0f8fcab17bc849a90ecd04b1f1d768d3a263bc6
sondang01 <zamsondang@gmail.com>
2020-04-13 00:11:39 +0800

    Belanjaan

---
+++ src/IfElse.java
@@ -0,0 +1,17 @@
+package percabangan;
+
+public class IfElse {
+    public static void main(String[] args) {
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
+
+        if(uangDiDompet<totalBelanja){
+            System.out.println("Uang kurang, kurangi jajan anda");
+        }else if(uangDiDompet>totalBelanja){
+            double kembalian = uangDiDompet-totalBelanja;
+            System.out.println("Uang cukup, kembalian : "+kembalian);
+        }else{
+            System.out.println("Uang pas.. MaNtUL..");
+        }
+    }
+}
