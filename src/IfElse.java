7e0383ff98696b73f9ea0b788221edd5238ee5c6
sondang01 <zamsondang@gmail.com>
2020-04-13 08:54:20 +0800

    If Else 3

--- src/IfElse.java
+++ src/IfElse.java
@@ -7,8 +7,11 @@ public class IfElse {
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi jajan anda");
+        }else if(uangDiDompet>totalBelanja) {
+            double kembalian = uangDiDompet - totalBelanja;
+            System.out.println("Uang cukup, kembalian : " + kembalian);
         }else{
-            System.out.println("Uang cukup, santuy");
+            System.out.println("Uang pas.. MaNaTuL..");
         }
     }
 }
