65b7c2d223ac542d79d0dd7f2b0a38af10be6523
sondang01 <zamsondang@gmail.com>
2020-04-13 15:03:14 +0800

    Switch 2

--- src/Percabangan/Switch.java
+++ src/Percabangan/Switch.java
@@ -1,23 +1,28 @@
 package percabangan;
 
 public class Switch {
-    public static void mian(String[] args) {
-        int jalanjalan=3;
-        switch (jalanjalan){
-            case 1:
-                System.out.println("Buaya");
+    public static void main(String[] args) {
+        char nilai='D';
+        switch (nilai){
+            case 'A':
+                System.out.println("Mhs     : \"Terima kasih pak\"");
+                System.out.println("Dosen    :\"Selamat ya !!\"");
                 break;
-            case 2:
-                System.out.println("Kadal");
+            case 'B':
+                System.out.println("Mhs      :\"Kenapa ulun kada dapata A, pak?\"");
+                System.out.println("Dosen      :\"#$@%#^\"");
                 break;
-            case 3:
-                System.out.println("Ayam-ayam");
-                break;
-            case 4:
-                System.out.println("Cicak");
+            case 'C':
+                System.out.println("Mhs      :\"Ulun turun trus pak ai, tugas juga pak saya kumpukl\"");
+                System.out.println("Dosen      :\"Tapi bisa lah menjawab ujian?\"");
+                System.out.println("Mhs        :\"HIhihIhihih..\"");
                 break;
             default:
-                System.out.println("Binatang");
+                System.out.println("Mhs     :\"Ulun turun trus pak ai, tugas juga pak saya kumpukl\"");
+                System.out.println("Dosen   :\"Bujur jua kah?\"");
+                System.out.println("Dosen    :\"*memeriksa berkas\"");
+                System.out.println("Dosen       :\"*menceleng\"");
+                System.out.println("Mhs     :\"Kabur....\"");
                 break;
         }
     }
